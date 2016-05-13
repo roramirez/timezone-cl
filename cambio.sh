9593a4392b93e4badcf2e9fae75f54b6f3840b23
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2016-05-13 16:18:51 -0300

    Remove hardcode year. Set year into var suggested by @twitter.com/pararayos

--- cambio.sh
+++ cambio.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-
+YEAR=$(date +"%Y")
 echo "Zona America/Santiago, antes del Cambio"
-zdump -v America/Santiago |grep 2015
+zdump -v America/Santiago |grep $YEAR
 echo "Realizando Cambio a la zona"
 zic southamerica
 echo "Zona America/Santiago, luego del Cambio"
-zdump -v America/Santiago |grep 2015
+zdump -v America/Santiago |grep $YEAR
 echo ""
 echo "Zona Chile/Continental, antes del Cambio"
-zdump -v Chile/Continental |grep 2015
+zdump -v Chile/Continental |grep $YEAR
 echo "Realizando Cambio a la zona"
 zic backward
-zdump -v Chile/Continental |grep 2015
+zdump -v Chile/Continental |grep $YEAR
 dpkg-reconfigure tzdata
 
